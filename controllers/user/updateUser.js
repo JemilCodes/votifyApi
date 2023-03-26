8021022b969f06e87244128c2947a19a09b99384
JemilCodes <jemiluishaq277@gmail.com>
2023-03-26 11:39:25 +0100

    updated user code

--- controllers/user/updateUser.js
+++ controllers/user/updateUser.js
@@ -3,21 +3,33 @@ const bcrypt = require("bcryptjs");
 
 const updateUser = async (req, res) => {
   const { userId } = req.cookies;
-  const { name, email, comfirm, password } = req.body;
+  const { oldPassword, newPassword } = req.body;
 
-  const prevData = await userModel.findById(userId);
-  const isPasswordCorrect = await bcrypt.compare(comfirm, prevData.password);
-  if (!isPasswordCorrect) res.status(401).json("incorrect password");
+  if ((oldPassword, newPassword)) {
+    const prevData = await userModel.findById(userId);
+    const isPasswordCorrect = await bcrypt.compare(
+      oldPassword,
+      prevData.password
+    );
+    if (!isPasswordCorrect) res.status(401).json("incorrect password");
 
-  const salt = await bcrypt.genSalt(10);
-  const hashsedPassword = await bcrypt.hash(password, salt);
+    const salt = await bcrypt.genSalt(10);
+    const hashsedPassword = await bcrypt.hash(newPassword, salt);
 
-  const data = await userModel.findByIdAndUpdate(
-    userId,
-    { name, email, password: hashsedPassword },
-    { new: true, runValidators: true }
-  );
-  return res.status(200).json(data);
+    const data = await userModel.findByIdAndUpdate(
+      userId,
+      { ...req.body, password: hashsedPassword },
+      { new: true, runValidators: true }
+    );
+    return res.status(200).json(data);
+  } else {
+    const data = await userModel.findByIdAndUpdate(
+      userId,
+      { ...req.body },
+      { new: true, runValidators: true }
+    );
+    return res.status(200).json(data);
+  }
 };
 
 module.exports = updateUser;
